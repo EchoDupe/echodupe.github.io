2795bfe0700cbf7d84b46fffb2e2682255021ad3
Skitxoe/Skit <tbasebc@gmail.com>
2026-03-07 15:19:13 -0500

    Update ranks.js

--- ranks.js
+++ ranks.js
@@ -1,5 +1,18 @@
 const rankData = [
     {
+        id: 'rank_sensor',
+        name: 'Sensor',
+        price: 3.00,
+        prefixRaw: '&8&l[&#FFFFFF&lS&#DFDFDF&lE&#B0B0B0&lN&#818181&lS&#525252&lO&#2D2D2D&lR&8&l]',
+        perks: [
+            'Exclusive Sensor Prefix',
+            '3 Homes: Set up to 3 teleport locations.',
+            'Fly in Spawn Access',
+            'Sensor Kit: (P5/U3)',
+            '3-Hour Kit Cooldown'
+        ]
+    },
+    {
         id: 'rank_stalker',
         name: 'Stalker',
         price: 4.00,
@@ -13,24 +26,13 @@ const rankData = [
         ]
     },
     {
-        id: 'rank_vanguard',
-        name: 'Vanguard',
-        price: 10.00,
-        perks: [
-            'Vanguard Tag', 
-            'Access to /fly', 
-            '2x Home Limit', 
-            'Priority Queue'
-        ]
-    },
-    {
         id: 'rank_warden',
         name: 'Warden',
         price: 5.00,
         prefixRaw: '&8&l[&#55FFFF&lW&#3BEDED&lA&#22DBDB&lR&#08C9C9&lD&#008E8E&lE&#001A33&lN&8&l]',
         perks: [
             'Exclusive Warden Prefix',
-            '5 Homes Set-locations',
+            '5 Homes: Set up to 5 teleport locations.',
             'Fly in Spawn Access',
             'Warden Kit (P6/U4)',
             '3-Hour Kit Cooldown'
