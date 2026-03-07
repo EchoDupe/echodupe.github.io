83a999a30b7c65b576bde4ac69e7dc1df3d3b690
Skitxoe/Skit <tbasebc@gmail.com>
2026-03-07 15:21:56 -0500

    Update ranks.js

--- ranks.js
+++ ranks.js
@@ -1,41 +1,41 @@
 const rankData = [
-    {
-        id: 'rank_sensor',
-        name: 'Sensor',
-        price: 3.00,
-        prefixRaw: '&8&l[&#FFFFFF&lS&#DFDFDF&lE&#B0B0B0&lN&#818181&lS&#525252&lO&#2D2D2D&lR&8&l]',
-        perks: [
-            'Exclusive Sensor Prefix',
-            '3 Homes: Set up to 3 teleport locations.',
-            'Fly in Spawn Access',
-            'Sensor Kit: (P5/U3)',
-            '3-Hour Kit Cooldown'
-        ]
-    },
-    {
-        id: 'rank_stalker',
-        name: 'Stalker',
-        price: 4.00,
-        prefixRaw: '&8&l[&#00FF88&lS&#00D572&lT&#00AB5C&lA&#008046&lL&#005531&lK&#003322&lE&#00110B&lR&8&l]',
-        perks: [
-            'Exclusive Stalker Prefix',
-            '4 Homes: Set up to 4 teleport locations.',
-            'Stalker Kit: Full Diamond Armor (P5/U4).',
-            'Fly in Spawn Access',
-            '3-Hour Kit Cooldown'
-        ]
-    },
-    {
-        id: 'rank_warden',
-        name: 'Warden',
-        price: 5.00,
-        prefixRaw: '&8&l[&#55FFFF&lW&#3BEDED&lA&#22DBDB&lR&#08C9C9&lD&#008E8E&lE&#001A33&lN&8&l]',
-        perks: [
-            'Exclusive Warden Prefix',
-            '5 Homes: Set up to 5 teleport locations.',
-            'Fly in Spawn Access',
-            'Warden Kit (P6/U4)',
-            '3-Hour Kit Cooldown'
-        ]
-    }
-];
+            {
+                id: 'rank_sensor',
+                name: 'Sensor',
+                price: 3.00,
+                prefixRaw: '&8&l[&#FFFFFF&lS&#DFDFDF&lE&#B0B0B0&lN&#818181&lS&#525252&lO&#2D2D2D&lR&8&l]',
+                perks: [
+                    'Exclusive Sensor Prefix',
+                    '3 Homes: Set up to 3 teleport locations.',
+                    'Fly in Spawn Access',
+                    'SENSOR KIT: FULL NETHERITE (P5|U3|Mending)',
+                    '3-Hour Kit Cooldown'
+                ]
+            },
+            {
+                id: 'rank_stalker',
+                name: 'Stalker',
+                price: 4.00,
+                prefixRaw: '&8&l[&#00FF88&lS&#00D572&lT&#00AB5C&lA&#008046&lL&#005531&lK&#003322&lE&#00110B&lR&8&l]',
+                perks: [
+                    'Exclusive Stalker Prefix',
+                    '4 Homes: Set up to 4 teleport locations.',
+                    'STALKER KIT: FULL NETHERITE (P5|U4|Mending)',
+                    'Fly in Spawn Access',
+                    '3-Hour Kit Cooldown'
+                ]
+            },
+            {
+                id: 'rank_warden',
+                name: 'Warden',
+                price: 5.00,
+                prefixRaw: '&8&l[&#55FFFF&lW&#3BEDED&lA&#22DBDB&lR&#08C9C9&lD&#008E8E&lE&#001A33&lN&8&l]',
+                perks: [
+                    'Exclusive Warden Prefix',
+                    '5 Homes Set-locations',
+                    'Fly in Spawn Access',
+                    'WARDEN KIT: FULL NETHERITE (P6|U4|Mending)',
+                    '3-Hour Kit Cooldown'
+                ]
+            }
+        ];
