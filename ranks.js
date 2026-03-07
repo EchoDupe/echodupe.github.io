16362c75313b131d759cbd145838803906a7f619
Skitxoe/Skit <tbasebc@gmail.com>
2026-03-07 15:03:49 -0500

    Update ranks.js

--- ranks.js
+++ ranks.js
@@ -25,11 +25,12 @@ export const rankData = [
         id: 'rank_warden',
         name: 'Warden',
         price: 5.00,
+        prefix: '<span style="color:#55FFFF">W</span><span style="color:#3BEDED">A</span><span style="color:#22DBDB">R</span><span style="color:#08C9C9">D</span><span style="color:#008E8E">E</span><span style="color:#001A33">N</span>',
         perks: [
             'Exclusive Warden Prefix',
             '5 Homes (/sethome)',
             'Fly in Spawn Access',
-            'Warden Kit (Prot 6 | Unbreaking 4)',
+            'Warden Kit (P6/U4)',
             '3-Hour Kit Cooldown'
         ]
     }
