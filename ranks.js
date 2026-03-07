aad15ae341098385f393b709c0f2225d1bcc58c2
Skitxoe/Skit <tbasebc@gmail.com>
2026-03-07 15:32:53 -0500

    Update ranks.js

--- ranks.js
+++ ranks.js
@@ -1,44 +1,59 @@
 const rankData = [
-            {
-                id: 'rank_sensor',
-                name: 'Sensor',
-                price: 3.00,
-                prefixRaw: '&8&l[&#FFFFFF&lS&#DFDFDF&lE&#B0B0B0&lN&#818181&lS&#525252&lO&#2D2D2D&lR&8&l]',
-                perks: [
-                    'Exclusive Sensor Prefix',
-                    '3 Homes: Set-locations',
-                    'Fly in Spawn Access',
-                    'KIT ARMOR: Netherite (P5|U3|Mend) [QUARTZ]',
-                    'KIT WEAPONS: Sword(S5) Axe(S5/E3) Pick(E5)',
-                    '3-Hour Kit Cooldown'
-                ]
-            },
-            {
-                id: 'rank_stalker',
-                name: 'Stalker',
-                price: 4.00,
-                prefixRaw: '&8&l[&#00FF88&lS&#00D572&lT&#00AB5C&lA&#008046&lL&#005531&lK&#003322&lE&#00110B&lR&8&l]',
-                perks: [
-                    'Exclusive Stalker Prefix',
-                    '4 Homes: Set-locations',
-                    'Fly in Spawn Access',
-                    'KIT ARMOR: Netherite (P5|U4|Mend) [EMERALD]',
-                    'KIT WEAPONS: Sword(S5) Axe(S5/E6) Pick(E5)',
-                    '3-Hour Kit Cooldown'
-                ]
-            },
-            {
-                id: 'rank_warden',
-                name: 'Warden',
-                price: 5.00,
-                prefixRaw: '&8&l[&#55FFFF&lW&#3BEDED&lA&#22DBDB&lR&#08C9C9&lD&#008E8E&lE&#001A33&lN&8&l]',
-                perks: [
-                    'Exclusive Warden Prefix',
-                    '5 Homes: Set-locations',
-                    'Fly in Spawn Access',
-                    'KIT ARMOR: Netherite (P6|U4|Mend) [DIAMOND]',
-                    'KIT WEAPONS: Sword(S6) Axe(S6/E6) Pick(E6)',
-                    '3-Hour Kit Cooldown'
-                ]
-            }
-        ];
+    {
+        id: 'rank_sensor',
+        name: 'Sensor',
+        price: 3.00,
+        prefixRaw: '&8&l[&#FFFFFF&lS&#DFDFDF&lE&#B0B0B0&lN&#818181&lS&#525252&lO&#2D2D2D&lR&8&l]',
+        global: [
+            'Exclusive Sensor Prefix', 
+            '3 Homes: Set-locations', 
+            'Fly in Spawn Access', 
+            '3-Hour Kit Cooldown'
+        ],
+        armor: 'Netherite Gear (P5|U3|M)',
+        trim: 'SENSOR | QUARTZ',
+        weapons: [
+            'Sword (S5|U3|M)',
+            'Axe (S5|U4|E3|M)',
+            'Pickaxe (E5|U3|M)'
+        ]
+    },
+    {
+        id: 'rank_stalker',
+        name: 'Stalker',
+        price: 4.00,
+        prefixRaw: '&8&l[&#00FF88&lS&#00D572&lT&#00AB5C&lA&#008046&lL&#005531&lK&#003322&lE&#00110B&lR&8&l]',
+        global: [
+            'Exclusive Stalker Prefix', 
+            '4 Homes: Set-locations', 
+            'Fly in Spawn Access', 
+            '3-Hour Kit Cooldown'
+        ],
+        armor: 'Netherite Gear (P5|U4|M)',
+        trim: 'SILENCE | EMERALD',
+        weapons: [
+            'Sword (S5|U4|M)',
+            'Axe (S5|U4|E6|M)',
+            'Pickaxe (E5|U4|M)'
+        ]
+    },
+    {
+        id: 'rank_warden',
+        name: 'Warden',
+        price: 5.00,
+        prefixRaw: '&8&l[&#55FFFF&lW&#3BEDED&lA&#22DBDB&lR&#08C9C9&lD&#008E8E&lE&#001A33&lN&8&l]',
+        global: [
+            'Exclusive Warden Prefix', 
+            '5 Homes: Set-locations', 
+            'Fly in Spawn Access', 
+            '3-Hour Kit Cooldown'
+        ],
+        armor: 'Netherite Gear (P6|U4|M)',
+        trim: 'WARD | DIAMOND',
+        weapons: [
+            'Sword (S6|U4|M)',
+            'Axe (S6|U4|E6|M)',
+            'Pickaxe (E6|U4|M)'
+        ]
+    }
+];
