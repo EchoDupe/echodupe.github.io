9293568e230743b88b12a1af538e86f88d11c043
Skitxoe/Skit <tbasebc@gmail.com>
2026-03-08 15:18:32 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ window.config = {
     serverName: "EchoDupeLS",
     serverIP: "play.echodupels.minehut.gg",
     serverPort: 25565,
-    discordLink: "https://discord.gg/yourlink",
+    discordLink: "https://discord.gg/aH2SyywD2x",
     cashAppTag: "$YourTagHere", 
     webhookUrl: "YOUR_WEBHOOK_URL_HERE",
     colors: { primary: "#06b6d4", background: "#000000" },
     status: { refreshInterval: 30000 },
     developedBy: "Skitxoe",
-    version: "1.2.0"
+    version: "2.0.0"
 };
